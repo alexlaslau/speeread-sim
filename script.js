3377b075fed5a32241e60df24e5db63c13e3954a
Alex Laslau <george_laslau@yahoo.com>
2024-07-14 00:16:40 +0300

    Add recursive speed increase and stop button

--- script.js
+++ script.js
@@ -11,40 +11,62 @@ arrayOfWords.forEach(function(word){
     textDiv.append(span);
 });
 
-var button = document.getElementById('btn');
-var isRunning = false;
+var startButton = document.getElementById('start-btn');
+var endButton = document.getElementById('end-btn');
+var textViteza = document.getElementById('viteza');
+var stopFlagReceived = false;
 
-button.addEventListener('click', function() {
-    var value = document.getElementById('vitezaCitire').value;
+startButton.addEventListener('click', function() {
+    var wordsPerMinute = document.getElementById('viteza-citire').value;
 
-    if(!value){
+    if (!wordsPerMinute) {
         console.log('Introdu viteza citire!');
         return;
     }
 
-    wordSelectionSpeed = 60000 / value;
-    console.log(`Time for word per minute - ${wordSelectionSpeed}`)
+    stopFlagReceived = false;
 
-    if (!isRunning){
-        isRunning = true;
-        const spans = textDiv.querySelectorAll('span');
-        let index = 0;
-    
-        const interval = setInterval(function() {
-            if (index < spans.length) {
-                if (index > 0)
-                    spans[index - 1].classList.remove('active');
-    
-                spans[index].classList.add('active');
-                index++;
-            } else {
-                clearInterval(interval);
-                setTimeout(function() {
-                    if (spans.length > 0)
-                        spans[spans.length - 1].classList.remove('active');
-                }, wordSelectionSpeed);
-                isRunning = false;
-            }
-        }, wordSelectionSpeed);
-    }
+    Run(parseInt(wordsPerMinute, 10) + 40);
 });
+
+endButton.addEventListener('click', function() {
+    stopFlagReceived = true;
+    textViteza.innerHTML = '0';
+});
+
+function Run(wordsPerMinute) {
+    textViteza.innerHTML = wordsPerMinute + ' cuvinte/minut';
+
+    wordInterval = 60000 / wordsPerMinute;
+
+
+    const spans = textDiv.querySelectorAll('span');
+    let index = 0;
+
+    const interval = setInterval(function() {
+        if (stopFlagReceived) {
+            clearInterval(interval);
+            if (index > 0) {
+                spans[index - 1].classList.remove('active');
+            }
+            return;
+        }
+
+        if (index < spans.length) {
+            if (index > 0)
+                spans[index - 1].classList.remove('active');
+
+            spans[index].classList.add('active');
+            index++;
+        } else {
+            clearInterval(interval);
+            setTimeout(function() {
+                if (spans.length > 0) {
+                    spans[spans.length - 1].classList.remove('active');
+                }
+            }, wordInterval);
+
+            Run(wordsPerMinute + 40);
+        }
+    }, wordInterval);
+}
